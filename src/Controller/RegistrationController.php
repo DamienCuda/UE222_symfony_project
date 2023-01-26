6b3cc151014bb5683b6edf7bc6cf6931c24ca862
Kaoutiz <94445745+Kaoutiz@users.noreply.github.com>
2023-01-26 17:57:18 +0100

    securisation login, register et espace membre

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -18,6 +18,11 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppAuthAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
+
+        if ($this->container->get('security.authorization_checker')->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('article_index');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
