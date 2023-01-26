0044ac7721fbe66a82ed974cb6206ba50914ff8b
Damien Cuda <contact@damiencuda.com>
2023-01-26 21:59:25 +0100

    style login

--- migrations/Version20230126201022.php
+++ migrations/Version20230126201022.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230126101242 extends AbstractMigration
+final class Version20230126201022 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -25,7 +25,7 @@ final class Version20230126101242 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\SqlitePlatform'."
         );
 
-        $this->addSql('CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, category_id INTEGER NOT NULL, title VARCHAR(255) NOT NULL COLLATE "BINARY", content CLOB NOT NULL COLLATE "BINARY", CONSTRAINT FK_23A0E6612469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, category_id INTEGER NOT NULL, title VARCHAR(255) NOT NULL COLLATE "BINARY", content CLOB NOT NULL COLLATE "BINARY", img CLOB DEFAULT NULL COLLATE "BINARY", published BOOLEAN DEFAULT NULL, date_publication CLOB DEFAULT NULL COLLATE "BINARY", CONSTRAINT FK_23A0E6612469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_23A0E6612469DE2 ON article (category_id)');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\SqlitePlatform,
@@ -33,6 +33,14 @@ final class Version20230126101242 extends AbstractMigration
         );
 
         $this->addSql('CREATE TABLE category (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL COLLATE "BINARY")');
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\SqlitePlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\SqlitePlatform'."
+        );
+
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(180) NOT NULL COLLATE "BINARY", roles CLOB NOT NULL COLLATE "BINARY" --(DC2Type:json)
+        , password VARCHAR(255) NOT NULL COLLATE "BINARY")');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
     }
 
     public function down(Schema $schema): void
@@ -50,5 +58,11 @@ final class Version20230126101242 extends AbstractMigration
         );
 
         $this->addSql('DROP TABLE category');
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\SqlitePlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\SqlitePlatform'."
+        );
+
+        $this->addSql('DROP TABLE user');
     }
 }
